7df6c273c4ea906c7a71c77e478964d7b8e5404b
Etienne Ameil <etienne@gmx.fr>
2017-07-24 11:57:53 +0200

    first blogger commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+  # Texte initial dans le fichier : seulement les 3 lignes ci dessous
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
+
+  # Texte donné dans le tuto
+Blogger::Application.routes.draw do
+  resources :articles
+end
